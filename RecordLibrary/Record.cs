6e2335ef65763d9fdbdf48400e2ee15dd52405e8
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-03-25 19:17:20 +0100

    Added Record class and unittestrecord class

--- RecordLibrary/Record.cs
+++ RecordLibrary/Record.cs
@@ -4,5 +4,38 @@ namespace RecordLibrary
 {
     public class Record
     {
+        private string _title;
+        private string _artist;
+        private int _releaseYear;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Error Record title","Record title can not be empty");
+                _title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Error Artist Name","Artist name can not be empty");
+                _artist = value;
+            }
+        }
+
+        public int ReleaseYear
+        {
+            get => _releaseYear;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Error Release Year Negative", "Value cant be a negative number");
+                _releaseYear = value;
+            }
+        }
     }
 }
--- UnitTestRecordLibrary/UnitTestRecord.cs
+++ UnitTestRecordLibrary/UnitTestRecord.cs
@@ -1,13 +1,48 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecordLibrary;
 
 namespace UnitTestRecordLibrary
 {
     [TestClass]
     public class UnitTestRecord
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Record _r1;
+
+        [TestInitialize]
+        public void Init()
         {
+            _r1 = new Record();
+        }
+
+        [TestMethod]
+        public void TestRecordTitle()
+        {
+
+            _r1.Title = "Our first album";
+
+            Assert.AreEqual(_r1.Title, "Our first album");
+            Assert.ThrowsException<ArgumentNullException>(() => _r1.Title = null);
+            Assert.ThrowsException<ArgumentNullException>(() => _r1.Title = "");
+        }
+
+        [TestMethod]
+        public void TestRecordArtist()
+        {
+            _r1.Artist = "Ghost Iris";
+
+            Assert.AreEqual(_r1.Artist, "Ghost Iris");
+            Assert.ThrowsException<ArgumentNullException>(() => _r1.Artist = null);
+            Assert.ThrowsException<ArgumentNullException>(() => _r1.Artist = "");
+        }
+
+        [TestMethod]
+        public void TestRecordReleaseYear()
+        {
+            _r1.ReleaseYear = 2020;
+
+            Assert.AreEqual(_r1.ReleaseYear, 2020);
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => _r1.ReleaseYear = -1 ));
         }
     }
 }
