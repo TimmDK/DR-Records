b5ddc417b98e39290be581bffc44e4a0d7638c1f
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-03-25 18:41:25 +0100

    added record library and added unittest for records

--- RecordLibrary/Record.cs
+++ RecordLibrary/Record.cs
@@ -2,7 +2,7 @@
 
 namespace RecordLibrary
 {
-    public class Class1
+    public class Record
     {
     }
 }
---
+++ UnitTestRecordLibrary/UnitTestRecord.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestRecordLibrary
+{
+    [TestClass]
+    public class UnitTestRecord
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
