0f7468dc4819448135f486eec2c5c646ee87a6cb
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-04-28 11:28:02 +0200

    Added a lot of shit

---
+++ RESTDRRecords/Controllers/RecordsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTDRRecords.Models;
+
+namespace RESTDRRecords.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private static readonly List<Record> _recordList = new List<Record>()
+        {
+            new Record("Record 1", "Artist 1", 2020),
+            new Record("Record 1", "Artist 2", 2010),
+            new Record("Record 2", "Artist 2", 2000),
+            new Record("Record 3", "Artist 3", 2010),
+            new Record("Record 4", "Artist 4", 2005)
+        };
+
+        // GET: api/Records
+        [HttpGet]
+        public IEnumerable<Record> Get()
+        {
+            return _recordList;
+        }
+
+        // GET: api/Records/5
+        [HttpGet("{title}", Name = "Get")]
+        public List<Record> Get(string title)
+        {
+            return _recordList.FindAll(i => i.Title == title);
+        }
+
+        // POST: api/Records
+        [HttpPost]
+        public void Post([FromBody] Record value)
+        {
+            _recordList.Add(value);
+        }
+
+        // PUT: api/Records/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RESTDRRecords/Models/Record.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTDRRecords.Models
+{
+    public class Record
+    {
+        private string _title;
+        private string _artist;
+        private int _releaseYear;
+
+        public Record()
+        {
+            
+        }
+
+        public Record(string title, string artist, int releaseYear)
+        {
+            _title = title;
+            _artist = artist;
+            _releaseYear = releaseYear;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Error Record title", "Record title can not be empty");
+                _title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Error Artist Name", "Artist name can not be empty");
+                _artist = value;
+            }
+        }
+
+        public int ReleaseYear
+        {
+            get => _releaseYear;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Error Release Year Negative", "Value cant be a negative number");
+                _releaseYear = value;
+            }
+        }
+    }
+}
